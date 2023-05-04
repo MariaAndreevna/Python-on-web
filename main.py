1eef17554b2c6ebc9a74489faae9a16f2e5cd139
MariaAndreevna <115341288+MariaAndreevna@users.noreply.github.com>
2023-05-04 13:15:20 +0300

    контрольная точка 5-6

--- main.py
+++ main.py
@@ -1,38 +1,51 @@
-from flask import Flask, render_template, request, escapefrom DBcm import UseDatabase
-import mysql.connector
-def search4letters(phrase: str, letters: str='aeiou') -> set:    return set(letters).intersection(set(phrase))
+from flask import Flask,render_template,request,escape,session
+from cheker import check_logged_in
+
+def log_request(req:'flask_request',res:str)->None:
+    with open('vsearch.log','a') as log:
+        print(req,res,file=log)
+
+def search4letters(pharse:str,letters:str='aeiou')->set:
+    return set(letters).intersection(set(pharse))
+
 app=Flask(__name__)
-@app.route('/')def hello()-> str:
-     return 'Hello world from Flask'
-@app.route('/viewlog')def view_the_log() -> 'html':
-    contents = []    with open('vsearch.log') as log:
-        for line in log:            contents.append([])
-            for item in line.split('|'):                contents[-1].append(escape(item))
-    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')    return render_template('viewlog.html',
-                           the_title='Viev Log',                           the_row_titles=titles,
-                           the_data=contents,)
-@app.route('/test')
-def test_page()->'html':    return render_template('test.html', the_title="Welcom")
-@app.route('/test-result', methods=['POST'])
-def testPost()->'html':    your_name=request.form['your_name']
-    return render_template('test-result.html', your_name=your_name)
-app.config['dbconfig'] = {'host': '127.0.0.1',                            'user': 'vsearch',
-                          'password': 'vsearchpasswd',                          'database': 'vsearchlogDB',}
-def log request(req: 'flask request', res: str) -> None:
-"""Log details of the web request and the results """        with UseDatabase(app.config['dbconfig']) as cursor:
-                SQL = """insert into log                (phrase, letters, ip, browser string, results) 
-                values (ts, ts, ts, ts, ts)"""                cursor.execute(SQL, (req.form['phrase'],
-                                     req.form['letters']                                    req.remote_addr,
-                                    req. user agent.browser,                                    res, ))
-@app.route('/search4', methods=['POST'])
-def do_search()->'html':    phrase = request.form['phrase']
-    letters = request.form['letters']    title = 'Here are your results:'
-    results = str(search4letters(phrase, letters))    log_request(request, results)
-    return render_template('results.html',                            the_phrase=phrase,
-                            the_letters=letters,                            the_title=title,
+app.secret_key='YouWillNeverGuess'
+@app.route('/search4',methods=['POST'])
+def do_search() -> 'html':
+    pharse=request.form['pharse']
+    letters=request.form['letters']
+    title='Here are your results:'
+    results=str(search4letters(pharse,letters))
+    log_request(request,results)
+    return render_template('result.html',
+                            the_title=title,
+                            the_pharse=pharse,
+                            the_letters=letters,
                             the_results=results)
+@app.route('/')
+@app.route('/entry')
+def entry_page() ->'html':
+    return render_template('entry.html',the_title='Welcome to search4letters on the web!')
+@app.route('/viewlog')
+@check_logged_in
+def view_the_log() -> 'html':
+    contents = []
+    with open('vsearch.log') as log:
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html', the_title='Viev Log', the_row_titles=titles,the_data=contents,)
 
+@app.route('/login')
+def do_loggin()->str:
+    session['logged_in']=True
+    return 'You are logged in'
 
-@app.route('/entry')def entry_page()->'html':
-    return render_template('entry.html', the_title="Welcom")
-app.run()
+@app.route('/logout')
+def do_logout()->str:
+    session.pop('logged_in')
+    return 'You are logged out'
+if __name__ == '__main__':
+    app.run(debug=True)
